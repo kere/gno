e1e312d731c79d0e41b2d006d871b115a99e1848
kere <a@kere.me>
2018-05-01 10:29:21 +0800

    builder_count.go

--- db/builder_count.go
+++ db/builder_count.go
@@ -1,6 +1,8 @@
 package db
 
-import "bytes"
+import (
+	"bytes"
+)
 
 // Counter builder
 type CounterBuilder struct {
@@ -12,17 +14,17 @@ func NewCounterBuilder(t string) *CounterBuilder {
 	return (&CounterBuilder{}).Table(t)
 }
 
-func (this *CounterBuilder) Table(t string) *CounterBuilder {
-	this.table = t
-	return this
+func (c *CounterBuilder) Table(t string) *CounterBuilder {
+	c.table = t
+	return c
 }
 
-func (this *CounterBuilder) Count(cond string, args ...interface{}) (int64, error) {
+func (c *CounterBuilder) Count(cond string, args ...interface{}) (int64, error) {
 	s := bytes.Buffer{}
-	database := this.getDatabase()
+	database := c.getDatabase()
 	// driver := database.Driver
 	s.WriteString("SELECT count(1) as count FROM ")
-	s.WriteString(this.table)
+	s.WriteString(c.table)
 
 	var r DataSet
 	var err error
@@ -39,4 +41,28 @@ func (this *CounterBuilder) Count(cond string, args ...interface{}) (int64, erro
 		return -1, err
 	}
 	return r[0].Int64("count"), nil
+}
+
+func (c *CounterBuilder) TxCount(tx *Tx, cond string, args ...interface{}) (int64, error) {
+	s := bytes.Buffer{}
+	// driver := database.Driver
+	s.WriteString("SELECT count(1) as count FROM ")
+	s.WriteString(c.table)
+
+	var row DataRow
+	var err error
+
+	if cond != "" {
+		s.Write(bSQLWhere)
+		s.WriteString(cond)
+		row, err = tx.QueryOne(NewSqlState(s.Bytes(), args...))
+	} else {
+		row, err = tx.QueryOne(NewSqlState(s.Bytes()))
+	}
+
+	if err != nil {
+		return -1, err
+	}
+
+	return row.Int64("count"), nil
 }
