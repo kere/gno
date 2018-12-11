9ecc23a25c6e2c4eee9546465bdb29b57acb3476
kere <a@kere.me>
2018-12-11 19:50:17 +0800

    a

--- libs/util/mapdata.go
+++ libs/util/mapdata.go
@@ -198,7 +198,11 @@ func (dr MapData) Int64(field string) int64 {
 	case string:
 		i, err := strconv.ParseInt(dr[field].(string), 10, 64)
 		if err != nil {
-			panic(err)
+			v, err := strconv.ParseFloat(dr[field].(string), 64)
+			if err != nil {
+				panic(err)
+			}
+			i = int64(v)
 		}
 		return i
 
