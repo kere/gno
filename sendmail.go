1727f8532ceafd73d56981f4eea2ff1dfbe6254e
kere <a@kere.me>
2018-12-23 13:43:40 +0800

    send tls mail

--- sendmail.go
+++ sendmail.go
@@ -4,6 +4,9 @@ import (
 	"bytes"
 	"encoding/base64"
 	"fmt"
+
+	"crypto/tls"
+	"net"
 	"net/mail"
 	"net/smtp"
 	"strings"
@@ -82,4 +85,137 @@ func (m *SMTPlainMail) Send(to []*mail.Address, title string, body []byte, mailT
 		m.from.Address,
 		mailList,
 		buf.Bytes())
+}
+
+//mailDial return a smtp client
+func mailDial(addr string) (*smtp.Client, error) {
+	conn, err := tls.Dial("tcp", addr, nil)
+	if err != nil {
+		return nil, err
+	}
+	//分解主机端口字符串
+	host, _, _ := net.SplitHostPort(addr)
+	return smtp.NewClient(conn, host)
+}
+
+// MailUser mail
+type MailUser struct {
+	Name string
+	Mail string
+}
+
+// MailTLS use tls
+type MailTLS struct {
+	Host string
+	Port int
+	From MailUser
+	Auth smtp.Auth
+}
+
+// NewMailTLS new
+func NewMailTLS(host string, port int, from MailUser, password string) MailTLS {
+	auth := newMailAuth(from.Mail, password)
+	return MailTLS{Host: host, Port: port, From: from, Auth: &auth}
+}
+
+//SendTo SendMail to one use TLS
+func (m *MailTLS) SendTo(to MailUser, title string, body []byte) error {
+	h := make(map[string]string)
+	usr := ""
+	if to.Name != "" {
+		usr = to.Name + "<" + to.Mail + ">"
+	} else {
+		usr = to.Mail
+	}
+	h["To"] = usr
+	toUsers := []string{usr}
+	return m.send(h, toUsers, title, body)
+}
+
+//Send SendMail to uses TLS
+func (m *MailTLS) Send(toName string, toAddrs []string, title string, body []byte) error {
+	h := make(map[string]string)
+	h["To"] = toName
+	return m.send(h, toAddrs, title, body)
+}
+
+// //Send SendMail use TLS
+// func (m *MailTLS) Send(toUserName string, to []string, title string, body []byte) (err error) {
+//   header["To"] =
+// }
+func (m *MailTLS) send(header map[string]string, to []string, title string, body []byte) (err error) {
+	header["From"] = m.From.Name + "<" + m.From.Mail + ">"
+	header["Subject"] = title
+	header["Content-Type"] = "text/html; charset=UTF-8"
+
+	buf := bytes.NewBuffer(nil)
+	for k, v := range header {
+		buf.WriteString(fmt.Sprintf("%s: %s\r\n", k, v))
+	}
+	buf.WriteString("\r\n")
+	buf.Write(body)
+
+	addr := fmt.Sprintf("%s:%d", m.Host, m.Port)
+	//create smtp client
+	c, err := mailDial(addr)
+	if err != nil {
+		return err
+	}
+	defer c.Close()
+	if m.Auth != nil {
+		if ok, _ := c.Extension("AUTH"); ok {
+			if err = c.Auth(m.Auth); err != nil {
+				return err
+			}
+		}
+	}
+	if err = c.Mail(m.From.Mail); err != nil {
+		return err
+	}
+	for _, addr := range to {
+		if err = c.Rcpt(addr); err != nil {
+			return err
+		}
+	}
+	w, err := c.Data()
+	if err != nil {
+		return err
+	}
+	_, err = w.Write(buf.Bytes())
+	if err != nil {
+		return err
+	}
+	err = w.Close()
+	if err != nil {
+		return err
+	}
+	return c.Quit()
+}
+
+type loginAuth struct {
+	username, password string
+}
+
+// newMailAuth func
+func newMailAuth(username, password string) loginAuth {
+	return loginAuth{username, password}
+}
+
+// Start Func
+func (a *loginAuth) Start(server *smtp.ServerInfo) (string, []byte, error) {
+	// return "LOGIN", []byte{}, nil
+	return "LOGIN", []byte(a.username), nil
+}
+
+// Next func
+func (a *loginAuth) Next(fromServer []byte, more bool) ([]byte, error) {
+	if more {
+		switch string(fromServer) {
+		case "Username:":
+			return []byte(a.username), nil
+		case "Password:":
+			return []byte(a.password), nil
+		}
+	}
+	return nil, nil
 }
