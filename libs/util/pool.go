0dff1871b4023e28515acc28ea93190a22c060bc
kere <a@kere.me>
2019-12-04 23:43:23 +0800

    data pool

--- libs/util/pool.go
+++ libs/util/pool.go
@@ -3,41 +3,29 @@ package util
 import "sync"
 
 var (
-	rowPool sync.Pool
-	colPool sync.Pool
+	rowPool    sync.Pool
+	colPool    sync.Pool
+	intsPool   sync.Pool
+	strsPool   sync.Pool
+	int64sPool sync.Pool
 )
 
 // GetRow from pool
 func GetRow() []float64 {
 	v := rowPool.Get()
-	return (v.([]float64))[:0]
+	if v == nil {
+		return make([]float64, 0, 100)
+	}
+	return v.([]float64)
 }
 
-// GetRowN from pool
+// GetRowN from pool, with 0 value
 func GetRowN(n int) []float64 {
 	v := rowPool.Get()
 	if v == nil {
 		return make([]float64, n)
 	}
 	row := v.([]float64)
-	l := len(row)
-	if n < l {
-		return row[:n]
-	}
-
-	for i := l; i < n; i++ {
-		row = append(row, 0)
-	}
-	return row
-}
-
-// GetRowN0 from pool, with 0 value
-func GetRowN0(n int) []float64 {
-	v := rowPool.Get()
-	if v == nil {
-		return make([]float64, n)
-	}
-	row := (v.([]float64))[:0]
 
 	for i := 0; i < n; i++ {
 		row = append(row, 0)
@@ -47,43 +35,27 @@ func GetRowN0(n int) []float64 {
 
 // PutRow to pool
 func PutRow(r []float64) {
-	rowPool.Put(r)
+	rowPool.Put(r[:0])
 }
+
+// ---------- float64 ----------
 
 // GetColumn from pool
 func GetColumn() []float64 {
 	v := colPool.Get()
 	if v == nil {
-		return []float64{}
+		return make([]float64, 0, 100)
 	}
-	return (v.([]float64))[:0]
+	return v.([]float64)
 }
 
-// GetColumnN from pool
+// GetColumnN from pool, with 0 value
 func GetColumnN(n int) []float64 {
 	v := colPool.Get()
 	if v == nil {
 		return make([]float64, n)
 	}
 	col := v.([]float64)
-	l := len(col)
-	if n < l {
-		return col[:n]
-	}
-
-	for i := l; i < n; i++ {
-		col = append(col, 0)
-	}
-	return col
-}
-
-// GetColumnN0 from pool, with 0 value
-func GetColumnN0(n int) []float64 {
-	v := colPool.Get()
-	if v == nil {
-		return make([]float64, n)
-	}
-	col := (v.([]float64))[:0]
 
 	for i := 0; i < n; i++ {
 		col = append(col, 0)
@@ -93,5 +65,95 @@ func GetColumnN0(n int) []float64 {
 
 // PutColumn to pool
 func PutColumn(r []float64) {
-	colPool.Put(r)
+	colPool.Put(r[:0])
+}
+
+// ---------- int64 ----------
+
+// GetInt64 from pool
+func GetInt64() []int64 {
+	v := int64sPool.Get()
+	if v == nil {
+		return make([]int64, 0, 100)
+	}
+	return v.([]int64)
+}
+
+// GetInt64N from pool, with 0 value
+func GetInt64N(n int) []int64 {
+	v := int64sPool.Get()
+	if v == nil {
+		return make([]int64, n)
+	}
+	arr := (v.([]int64))
+
+	for i := 0; i < n; i++ {
+		arr = append(arr, 0)
+	}
+	return arr
+}
+
+// PutInt64 to pool
+func PutInt64(r []int64) {
+	int64sPool.Put(r[:0])
+}
+
+// ---------- int ----------
+
+// GetInt from pool
+func GetInt() []int {
+	v := intsPool.Get()
+	if v == nil {
+		return make([]int, 0, 100)
+	}
+	return v.([]int)
+}
+
+// GetIntN from pool, with 0 value
+func GetIntN(n int) []int {
+	v := intsPool.Get()
+	if v == nil {
+		return make([]int, n)
+	}
+	arr := v.([]int)
+
+	for i := 0; i < n; i++ {
+		arr = append(arr, 0)
+	}
+	return arr
+}
+
+// PutInt to pool
+func PutInt(r []int) {
+	intsPool.Put(r[:0])
+}
+
+// ---------- strings ----------
+
+// GetStrings from pool
+func GetStrings() []string {
+	v := intsPool.Get()
+	if v == nil {
+		return make([]string, 0, 100)
+	}
+	return v.([]string)
+}
+
+// GetStringsN from pool, with 0 value
+func GetStringsN(n int) []string {
+	v := strsPool.Get()
+	if v == nil {
+		return make([]string, n)
+	}
+	arr := v.([]string)
+
+	for i := 0; i < n; i++ {
+		arr = append(arr, "")
+	}
+	return arr
+}
+
+// PutStrings to pool
+func PutStrings(r []string) {
+	strsPool.Put(r[:0])
 }
